7fc009fbbe17cdde0ac7335bd420455c3d5e305b
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-01-30 14:43:11 +0530

    Algorithm.Basics - Modify the sequence

---
+++ src/Algorithm/Basics/ModifyTheSeq.java
@@ -0,0 +1,24 @@
+package Algorithm.Basics;
+
+import java.util.Scanner;
+
+/**
+ * Created by zub on 29/01/2017.
+ */
+public class ModifyTheSeq {
+    public static void ModifyTheSeq(){
+
+        Scanner scan = new Scanner(System.in);
+        int noOfDays = scan.nextInt();
+        int addReach = 0;
+        int sharedTo = 5;
+        for (int i = 1; i <= noOfDays; i++){
+            int likedCurrentDay = Math.floorDiv(sharedTo,2);
+            int sharedCurrentDay = likedCurrentDay*3;
+            addReach = addReach + likedCurrentDay;
+            sharedTo = sharedCurrentDay;
+
+        }
+        System.out.println(addReach);
+    }
+}
