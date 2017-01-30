6465c56496208284eb380704d9342fa26a7852ae
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-01-30 14:44:28 +0530

    Algorithm Basics - Sock Merchant
    https://www.hackerrank.com/challenges/sock-merchant

--- src/Algorithm/Basics/SockMerchant.java
+++ src/Algorithm/Basics/SockMerchant.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class SockMerchant {
     public static void SockMerchant(){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int c[] = new int[n];
-        for(int c_i=0; c_i < n; c_i++){
-            c[c_i] = in.nextInt();
+        int totalUnfilteredSocks = in.nextInt();
+        int unfilteredSocksAr[] = new int[totalUnfilteredSocks];
+        for(int c_i=0; c_i < totalUnfilteredSocks; c_i++){
+            unfilteredSocksAr[c_i] = in.nextInt();
         }
-        int count = 0;
-        for (int p = 0; p < n; p++){
-            for (int q = p + 1; q < n; q++){
-                if (c[p] == c[q] && c[p] != 0 && c[q] != 0){
-                    count++;
-                    c[p] = 0;
-                    c[q] = 0;
+        int matchedSocksPair = 0;
+        for (int p = 0; p < totalUnfilteredSocks; p++){
+            for (int q = p + 1; q < totalUnfilteredSocks; q++){
+                if (unfilteredSocksAr[p] == unfilteredSocksAr[q] && unfilteredSocksAr[p] != 0 && unfilteredSocksAr[q] != 0){
+                    matchedSocksPair++;
+                    unfilteredSocksAr[p] = 0;
+                    unfilteredSocksAr[q] = 0;
                     break;
                 }
             }
         }
-        System.out.println(count);
+        System.out.println(matchedSocksPair);
     }
 }
