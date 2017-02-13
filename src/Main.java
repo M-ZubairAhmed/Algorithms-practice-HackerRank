a27afcde9f1ac4718bc4871b01bbe7c0a6c1bdec
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-02-13 15:07:13 +0530

    random save

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,6 @@
-
+import static JavaLang.Introduction.JavaOutputFormatting.JavaOutputFormatting;
 public class Main {
-
     public static void main(String[] args) {
-//        hackerrank_plusminus();
-//        hackerrank_staircase();
-//        TimeConversion();
-//        Simplesum();
-//        CircularArrayRotation();
-//        CamelCase();
-//        ExtraLongFactorials();
-//        TimeInWords();
-//        MinMaxSum();
-//        UtopianTree();
-//        ArraysDS();
-//        MaximumDraw(); not yet completed
-//        Kangaroo();
-//        DivisibleSumPairs();
-//        DiagonalDifference();
-//        SockMerchant();
-//        AngryProfessor();
-//        FindDigits();
-//        AppendDelete(); not yet completed
-//        BonAppetit();
-//        HumanReadableTime();
-//        DesignerPDFViewer();
-//        AppleOrange();
-//        BeautifulDays();
-//        BetweenTwoSets();
-//        CompareTriplets();
-//        NewYearChaos();
-//        ModifyTheSeq();
-//        SuperReducedStrings();
-//        Panagram();
-//        CaesarCipher();
-//        MarsExploration();
-//        FunnyStrings();
-//        Gemstones();
-//        AlternatingCharacters();
-//        GameofThrones_I();
-//        Encryption();
-//        ModifiedKaprekarNumber();
-//        SherlockValidString();
-//        HappyLadyBugs();
-//        LibraryFine();
-//        CutTheSticks();
-//        BeautifulTriplets();
-//        SherlockAndSquare();
-//        LoveLetterMystery();
-//        BinaryBeautifulString();
-//        PalindromeIndex();
-//        TwoString();
-//        MigratoryBird();
-//        MinimumAbsoluteDifferenceInArray();
-//        HackerRankInString();
+        JavaOutputFormatting();
     }
 }
